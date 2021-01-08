e14f6d7c2dd3a3a485a237d2e0de0049b7686308
eveling <e.giselle@hotmail.com>
2021-01-07 21:53:28 -0500

    Crear efecto parallax

---
+++ js/parallax.js
@@ -0,0 +1,32 @@
+
+//Propiedades
+var propParallax = {
+
+	seccion: document.querySelector('.parallax'),
+	recorrido: null,
+	limite: null
+
+}
+
+
+//Metodos
+var metParallax = {
+	inicio: function () {
+		window.addEventListener('scroll', metParallax.parallax);
+	},
+
+	parallax: function () {
+		propParallax.recorrido = pageYOffset; 
+		propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+
+		if (propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite){
+			propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 3 + 'px';
+		}
+
+		else{
+			propParallax.seccion.style.backgroundPositionY = 0;
+		}
+	}
+}
+
+metParallax.inicio();

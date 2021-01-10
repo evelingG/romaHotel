2d99cf664d4c7b778dbb28c89ff63dba8d32f204
eveling <e.giselle@hotmail.com>
2021-01-10 11:35:05 -0500

    Editar efecto parallax

--- js/parallax.js
+++ js/parallax.js
@@ -1,32 +1,35 @@
+(function(){
 
-//Propiedades
-var propParallax = {
+	//Propiedades
+	var propParallax = {
 
-	seccion: document.querySelector('.parallax'),
-	recorrido: null,
-	limite: null
+		seccion: document.querySelector('.parallax'),
+		recorrido: null,
+		limite: null
 
-}
+	}
 
 
-//Metodos
-var metParallax = {
-	inicio: function () {
-		window.addEventListener('scroll', metParallax.parallax);
-	},
+	//Metodos
+	var metParallax = {
+		inicio: function () {
+			window.addEventListener('scroll', metParallax.parallax);
+		},
 
-	parallax: function () {
-		propParallax.recorrido = pageYOffset; 
-		propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+		parallax: function () {
+			propParallax.recorrido = pageYOffset; 
+			propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
 
-		if (propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite){
-			propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 3 + 'px';
-		}
+			if (propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite){
+				propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 3 + 'px';
+			}
 
-		else{
-			propParallax.seccion.style.backgroundPositionY = 0;
+			else{
+				propParallax.seccion.style.backgroundPositionY = 0;
+			}
 		}
 	}
-}
 
-metParallax.inicio();
+	metParallax.inicio();
+	
+}())
